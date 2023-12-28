314a15963f0d62e42f935ebf92d6d1c45d5b7497
Jubin Ben <jubinben.666@gmail.com>
2023-12-28 15:11:10 +0530

    Updated the UI

--- main.py
+++ main.py
@@ -1,72 +1,80 @@
 import streamlit as st
 import unicodedata
-from itertools import chain
 
-def empty_space(height: int):
-    for _ in range(height): st.write('\n')
+from datetime import datetime
+from patterns import PATTERN_DICT
+from utils import empty_space
+from itertools import chain
 
 def handleClick():
     st.session_state.current_index += 1
 
-def input_dict(pattern:str, example_nihongo:list, example_english:list) -> dict:
-    return {
-        "pattern": pattern,
-        "example": example_nihongo,
-        "example_english": example_english
+# page config
+st.set_page_config(
+    page_title="日本語-patterns",
+    menu_items={
+        "Report a Bug": "mailto:jubinben@gmail.com",
+        "About": f"""
+                  #### Made with :heart: by Jubin Ben :)
+                  ###### Deployed on :red[{datetime.now().strftime("%d-%m-%Y %H:%M")}]
+                  """
     }
+)
 
 # title
-st.write(""" # 日本語　Pattern Revision JLPT-N5""")
-empty_space(3)
+st.write("""
+# 日本語 Pattern Revision JLPT-N5 
+""")
+st.caption(f"""
+Deployed on :red[{datetime.now().strftime("%d-%m-%Y")}]
+""")
 
-# select chapters
-st.write(""" ## Range of chapters to revise""")
-chapters = st.slider('chapter-slider', 1, 10, (1, 2), label_visibility="hidden")
+empty_space(st, 3)
+with st.container(border=True):
+    # select chapters
+    st.write("""### Range of chapters to revise""",)
+    chapters = st.slider('chapter-slider', 1, 10, (1, 2), label_visibility="hidden")
 
-# patterns dictionary from each chapter
-pattern_dict = {
-    1: [
-        input_dict("[N1] wa [N2] desu", ["わたしわ　マイク　ミラ　です。"], ["I'm Mike Miller."]),
-        input_dict("[N1] wa [N2] ja (dewa) arimansen", ["サントスさんは　がくせい　じゃありません。"], ["Mr. Santosh is not a student."]),
-        input_dict("[N1] wa [N2] desu ka", ["ミラさんは　アメリカじん　ですか。"], ["Is Mira san American?"]),
-        input_dict("[N] mo", ["ミラさんは　かいしゃいん　です。グプタさんも　かいしゃいん　です。"], ["Mira san is company employee. Mr. Gupta san is also company employee."]),
-        input_dict("[N1] no [N2]", ["ミラさんは　IMC　の　しゃいん　です。"], ["Mr. Mira is an employee of IMC."]),
-    ],
-    2:[
-        input_dict("[N1] no [N2] (for things)", ["これは　コンピュタ　の　ほん　です。"], ["This is a book on computer."]),
-    ]
-}
+    dict_keys = list(PATTERN_DICT.keys())
+    patterns = []
+    if chapters[0] in dict_keys and chapters[1] in dict_keys:
+        patterns = list(chain(*[PATTERN_DICT[i] for i in range(chapters[0], chapters[1] + 1)]))
+    else:
+        st.write(f"##### Please select chapter range from {dict_keys[0]} to {dict_keys[-1]}")
 
-dict_keys = list(pattern_dict.keys())
-patterns = []
-if chapters[0] in dict_keys and chapters[1] in dict_keys:
-    patterns = list(chain(*[pattern_dict[i] for i in range(chapters[0], chapters[1]+1)]))
-else:
-    st.write(f"##### Please select chapter range from {dict_keys[0]} to {dict_keys[-1]}")
+    # Show and revise the patterns
+    if "current_index" not in st.session_state:
+        st.session_state.current_index = 0
 
-# Show and revise the patterns
-if "current_index" not in st.session_state:
-    st.session_state.current_index = 0
+    empty_space(st, 3)
+    if chapters[0] > 0 and st.session_state.current_index < len(patterns) and len(patterns) > 0:
+        tab1, tab2, tab3 = st.tabs(["##### Pattern", "##### Japanese example", "##### English example"])
+        with tab1:
+            st.write(f"#### &nbsp; {patterns[st.session_state.current_index]['pattern']}")
+        with tab2:
+            for i in patterns[st.session_state.current_index]['example']:
+                st.write(f"#### &nbsp; {unicodedata.normalize('NFKC', i)}")
+        with tab3:
+            for i in patterns[st.session_state.current_index]['example_english']:
+                st.write(f"#### &nbsp; {i}")
 
-empty_space(3)
-if chapters[0] > 0 and st.session_state.current_index < len(patterns) and len(patterns) > 0:
-    tab1, tab2, tab3 = st.tabs(["Pattern", "Japanese example", "English example"])
-    with tab1:
-        st.write(f"### {patterns[st.session_state.current_index]['pattern']}")
-    with tab2:
-        for i in patterns[st.session_state.current_index]['example']:
-            st.write(f"### {unicodedata.normalize('NFKC', i)}")
-    with tab3:
-        for i in patterns[st.session_state.current_index]['example_english']:
-            st.write(f"### {i}")
+        empty_space(st, 2)
+        submit = st.button('Next Pattern &nbsp; :arrow_forward:', on_click=handleClick)
+        empty_space(st, 1)
 
-    submit = st.button('Next Pattern', on_click=handleClick)
+    # finish message
+    if st.session_state.current_index == len(patterns):
+        empty_space(st, 5)
+        st.text("Patterns finished, press the Reset button to start over.")
+        empty_space(st, 6)
+    st.empty()
 
-st.empty()
-
-empty_space(6)
-if st.button("Reset", type="primary"):
-    st.session_state.current_index = 0
-    st.rerun()
+# reset button & re-run
+empty_space(st, 3)
+_, _, col, _, _ = st.columns(5)
+with col:
+    if st.button("Reset", type="primary"):
+        st.session_state.current_index = 0
+        st.rerun()
 
 
