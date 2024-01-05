bb65fe626487236211ee95e5dc73362463462096
Jubin Ben <jubinben.666@gmail.com>
2024-01-05 22:22:37 +0530

    Added the pattersn from lesson 7 and 8.

--- patterns.py
+++ patterns.py
@@ -34,5 +34,20 @@ PATTERN_DICT = {
         input_dict(":blue[V] ませんか。", ["いしょに　ひるごはん　を　たべませんか。"], ["Would you like to have lunch with us."]),
         input_dict(":blue[V] ましょう。", ["きょと　へ　いきましょう。"], ["Let's go to Kyoto."]),
     ],
-
+    7: [
+        input_dict(":orange[N (tool/means)] で :blue[V]", ["はし　で　たべます。", "日本でリポトをかきます。"], ["I eat with chopsticks.", "I'm going to write the report in Japanese."]),
+        input_dict("'Word/Sentence'は　ーで何ですか。", ["’Thank you’は　日本ごで何ですか。"], ["What is 'Thank you' in Japanese?"]),
+        input_dict(":orange[N1]に :orange[N2]　を　あげます/かします/おしえます", ["わたしは　チッカちゃん　に　はなっを　あげました。"], ["I gave flowers to Tikka."]),
+        input_dict(":orange[N1]に :orange[N2]　を　もらいます/かります/ならいます", ["わたしは　チッカちゃん　に　日本ご　を　ならいます。"], ["I'm learning Japanese from Tikka."]),
+        input_dict("もう　:blue[Vました]", ["父は　もう　ねました。"], ["Dad already slept."]),
+    ],
+    8: [
+        input_dict(":orange[N]は　な-adj(without な)　です。|| :orange[N]は　い-adj(~い)　です。", ["せんせいは　しんせつ　です。", "しごとは　おもしろい　です。"], ["Teacher is kind.", "Work is interesting."]),
+        input_dict("な-adj(な)　:orange[N] || い-adj(~い) :orange[N]", ["せんせいは　しんせつな　ひと　です。", "ならは　ふるい　まち　です。"], ["Teacher is a kind person.", "Nara is an old city."]),
+        input_dict("〜が、〜", ["日本の　たべものは　おいしい　です　が　たかい　です。"], ["Japanese food is tasty but expensive."]),
+        input_dict("とても/あまり", ["ワンさんは　とても　きれい　です。", "ワンさんは　あまり　きれい　じゃありません。"], ["Mr. Wan is beautiful.", "Mr. Was is not very beautiful."]),
+        input_dict("な-adj(without な) じゃありません　|| い-adj　くないです。", ["ワンさんは　きれい　です。", "この　かいしゃは　あたらしくないです。"], ["Mr. Wan is beautiful.", "This company is not new."]),
+        input_dict(":orange[N]は　どうですか。", ["日本のたべものは　どうですか。"], ["How is the Japanese food?"]),
+        input_dict(":orange[N1]は　どんな :orange[N2] ですか。", ["ワンさんは　どんな　ひとですか。"], ["How is Mr. Wan as a person?"]),
+    ],
 }
